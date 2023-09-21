2cdcdb4f30981f0992150adf54a6f6dcb2ef2eaf
greatbrick <speharley@gmail.com>
2023-09-20 20:15:19 -0400

    Change Player Starting Position

--- Assets/General Player/PlayerMovement.cs
+++ Assets/General Player/PlayerMovement.cs
@@ -138,11 +138,13 @@ public class PlayerMovement : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        inputtedLookDirection = Vector2.zero;
     }
 
     public void SetDefaultMovementEnabled(bool newValue)
     {
         defaultMovementEnabled = newValue;
+        rb.useGravity = newValue;
     }
 
     private void SetUpCamera()
