5e8832a16c3eabf1c9ea6d5dfe35e4bd9dfd4dd3
greatbrick0 <speharley@gmail.com>
2023-09-22 16:43:53 -0400

    Force Based Movement

---
+++ Assets/Scripts/QuickMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float speed = 5.0f;
+    [SerializeField]
+    float turnSpeed = 2.0f;
+    [SerializeField]
+    float jumpForce = 10.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = 5;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) rb.AddForce(transform.forward * speed, ForceMode.Force);
+        if (Input.GetKey(KeyCode.S)) rb.AddForce(-transform.forward * speed, ForceMode.Force);
+        if (Input.GetKey(KeyCode.A)) rb.AddForce(-transform.right * speed, ForceMode.Force);
+        if (Input.GetKey(KeyCode.D)) rb.AddForce(transform.right * speed, ForceMode.Force);
+
+        if (Input.GetKeyDown(KeyCode.Space)) rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+
+        if (Input.GetKey(KeyCode.Q)) transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.E)) transform.Rotate(-Vector3.up * turnSpeed * Time.deltaTime);
+
+    }
+}
