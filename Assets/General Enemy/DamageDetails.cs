14a50c95d952c7ce2e2e8f02f64c9b1047b0e036
greatbrick0 <speharley@gmail.com>
2023-10-07 18:25:56 -0400

    Added Damageable Class

---
+++ Assets/General Enemy/DamageDetails.cs
@@ -0,0 +1,25 @@
+
+namespace DamageDetails
+{
+    public enum DamageSource
+    {
+        Bullet,
+        Ability,
+        Environment
+    }
+
+    public enum DamageSpot
+    {
+        Head,
+        Body,
+        Armour
+    }
+
+    public enum DamageElement
+    {
+        Normal,
+        Fire,
+        Ice,
+        Eldritch
+    }
+}
