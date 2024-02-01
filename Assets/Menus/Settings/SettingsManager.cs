f82ee97ed7d8fb8831df6613e3220a2cf72448af
conorMaccarthy <conor.maccarthy@ontariotechu.net>
2024-01-31 19:54:20 -0500

    Audio settings working properly

--- Assets/Menus/Settings/SettingsManager.cs
+++ Assets/Menus/Settings/SettingsManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
     [SerializeField] Camera gameCamera;
     
+    //DISPLAY SETTINGS
     public void SetResolution(int width, int height)
     {
         Screen.SetResolution(width, height, Screen.fullScreenMode);
@@ -29,5 +32,33 @@ public class SettingsManager : MonoBehaviour
     public void SetFOV(int fov)
     {
         gameCamera.fieldOfView = fov;
+    }
+
+
+    //AUDIO SETTINGS
+    [SerializeField] TextMeshProUGUI masterDisplay;
+    [SerializeField] TextMeshProUGUI soundsDisplay;
+    [SerializeField] TextMeshProUGUI musicDisplay;
+    [SerializeField] TextMeshProUGUI menuDisplay;
+    public void SetMasterVolume(float volumeLevel)
+    {
+        AudioManager.instance.masterVolume = volumeLevel;
+        masterDisplay.text = Mathf.Round(volumeLevel * 100).ToString();
+    }
+    public void SetSoundsVolume(float volumeLevel)
+    {
+        AudioManager.instance.SFXVolume = volumeLevel;
+        AudioManager.instance.ambienceVolume = volumeLevel;
+        soundsDisplay.text = Mathf.Round(volumeLevel * 100).ToString();
+    }
+    public void SetMusicVolume(float volumeLevel)
+    {
+        AudioManager.instance.musicVolume = volumeLevel;
+        musicDisplay.text = Mathf.Round(volumeLevel * 100).ToString();
+    }
+    public void SetMenuVolume(float volumeLevel)
+    {
+        //Doesn't exist currently
+        menuDisplay.text = Mathf.Round(volumeLevel * 100).ToString();
     }
 }
