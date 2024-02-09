44f2a322c9f91c49f262db678ba33bd6c21a93a8
conorMaccarthy <conor.maccarthy@ontariotechu.net>
2024-02-09 18:31:06 -0500

    Dynamic outline now appears

--- Assets/Menus/StartMenu/LevelPanel.cs
+++ Assets/Menus/StartMenu/LevelPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class LevelPanel : MonoBehaviour, IPointerEnterHandler
@@ -8,13 +9,20 @@ public class LevelPanel : MonoBehaviour, IPointerEnterHandler
     [SerializeField] GameObject labelImage;
     [SerializeField] RectTransform rectTransform;
     [SerializeField] List<LevelPanel> otherPanels;
+    [SerializeField] Image outline;
     public bool isActive;
+
+    private void Update()
+    {
+        outline.GetComponent<RectTransform>().sizeDelta = new Vector2(rectTransform.sizeDelta.x + 10, rectTransform.sizeDelta.y + 10);
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (!isActive)
         {
             LeanTween.value(300, 600, 0.1f).setOnUpdate(AdjustWidth);
+            LeanTween.value(0, 1, 0.1f).setOnUpdate(AdjustOutlineOpacity);
             LeanTween.moveLocalX(labelImage, 0, 0.1f);
             isActive = true;
         }
@@ -32,11 +40,17 @@ public class LevelPanel : MonoBehaviour, IPointerEnterHandler
     public void Deactivate()
     {
         LeanTween.value(600, 300, 0.1f).setOnUpdate(AdjustWidth);
+        LeanTween.value(1, 0, 0.1f).setOnUpdate(AdjustOutlineOpacity);
         LeanTween.moveLocalX(labelImage, -300, 0.1f);
     }
 
     public void AdjustWidth(float val)
     {
         rectTransform.sizeDelta = new Vector2(val, 360);
+    }
+
+    public void AdjustOutlineOpacity(float val)
+    {
+        outline.color = new Color(1, 1, 1, val);
     }
 }
