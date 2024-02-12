234462d79870dec80c1b5ab091672c2442cb274d
greatbrick <speharley@gmail.com>
2024-02-12 06:19:13 -0500

    Continuous Enemy Spawning

--- Assets/Level/SpawnDataHolder.cs
+++ Assets/Level/SpawnDataHolder.cs
@@ -14,11 +14,32 @@ public class SpawnDataHolder : MonoBehaviour
     private bool firstRoom;
     [SerializeField]
     private List<EnemySpot> continuousSpawns;
+    private bool continueSpawning = false;
+    [SerializeField]
+    [Tooltip("The amount of enemiess that will be spawned before stopping. Setting to 0 will disable the limit.")]
+    private int maxEnemies;
+    private int spawnedEnemyCount = 0;
+    [SerializeField]
+    [Tooltip("The amount of that will pass before stopping spawning enemies, measured in seconds. Setting to 0 will disable the limit. ")]
+    private float maxDuration;
+    private float spawningDuration = 0.0f;
+    private float timeSinceLastSpawn = 0.0f;
+    [SerializeField]
+    [Tooltip("The time until the next enemy spawn, measured in seconds. Value taken from seconds passed since spawning started. ")]
+    private AnimationCurve spawnFrequency;
+    [SerializeField]
+    [Tooltip("The time spawnFrequency is extended over.")]
+    private float frequencyCurveDuration;
+    [SerializeField]
+    [Tooltip("The frrequency used when frequencyCurveDuration has passed. ")]
+    private float beyondCurveFrequency;
+    private float timeUntilNextSpawn;
 
     [Serializable] public class EnemySpot
     {
         public Vector3 position;
         public List<GameObject> supportedTypes;
+        public float weight = 1;
     }
 
     private void Start()
@@ -27,12 +48,45 @@ public class SpawnDataHolder : MonoBehaviour
         if (firstRoom) InitialWaveSpawn();
     }
 
+    private void Update()
+    {
+        if (!continueSpawning) return;
+        spawningDuration += 1.0f * Time.deltaTime;
+
+        timeSinceLastSpawn += 1.0f * Time.deltaTime;
+        if(timeSinceLastSpawn >= timeUntilNextSpawn)
+        {
+            timeSinceLastSpawn = 0.0f;
+            if (spawningDuration >= frequencyCurveDuration) timeUntilNextSpawn = beyondCurveFrequency;
+            else timeUntilNextSpawn = spawnFrequency.Evaluate(spawningDuration / frequencyCurveDuration);
+            print(timeUntilNextSpawn);
+            SpawnSingleEnemy();
+        }
+
+        if (spawnedEnemyCount >= maxEnemies && maxEnemies > 0) continueSpawning = false;
+        if (spawnedEnemyCount >= maxEnemies && maxEnemies > 0) continueSpawning = false;
+    }
+
     public void InitialWaveSpawn()
     {
         foreach(EnemySpot ii in initialSpawns)
         {
             enemySpawner.SpawnEnemy(ii.position + transform.position, ii.supportedTypes[0]);
-            print("should: " + (ii.position + transform.position));
+            spawnedEnemyCount += 1;
         }
+        if (spawnedEnemyCount < maxEnemies) continueSpawning = true;
+    }
+
+    public void SpawnSingleEnemy()
+    {
+        spawnedEnemyCount += 1;
+        EnemySpot chosenSpot = continuousSpawns[Mathf.FloorToInt(spawningDuration * 0.71f) % continuousSpawns.Count];
+        GameObject chosenType = chosenSpot.supportedTypes[Mathf.FloorToInt(spawningDuration * 1.22f) % chosenSpot.supportedTypes.Count];
+        enemySpawner.SpawnEnemy(chosenSpot.position + transform.position, chosenType);
+    }
+
+    public void DisableSpawning()
+    {
+        continueSpawning = false;
     }
 }
