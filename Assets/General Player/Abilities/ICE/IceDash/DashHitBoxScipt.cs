b0d78df8ed8979152de4d7a4d5eae6487c334206
greatbrick <speharley@gmail.com>
2024-02-16 10:43:54 -0500

    App Now Runs In Background
    Thank you to the guy from the feedback form that recommended that we do more calculations server-side. We will not do that.

---
+++ Assets/General Player/Abilities/ICE/IceDash/DashHitBoxScipt.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashHitBoxScipt : MonoBehaviour
+{
+
+    public PlayerSpellIceDash dashAbilityRef;
+    public int abilityDamage;
+    DamageNumberManager hitNumberRef;
+
+    public List<Damageable> hitTargets;
+
+    private void Start()
+    {
+        hitNumberRef = DamageNumberManager.GetManager();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var hitbox = other.GetComponent<Hitbox>();
+        if (hitbox != null)
+        {
+            foreach (Damageable ii in hitTargets)
+            {
+                if (hitbox.GetOwner() == ii)
+                {
+                    Debug.Log("Already hit this enemy");
+                    return;
+                }
+            }
+            if (hitbox.GetOwner().team == "Enemy")
+            {
+                var spot = hitbox.GetSpotType();
+                int hit = hitbox.GetOwner().TakeDamage(abilityDamage, DamageDetails.DamageSource.Ability, hitbox.GetSpotType());
+                Vector3 location = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+                hitNumberRef.CreateDamageNumber(hit,location,DamageDetails.DamageElement.Ice, spot);
+                hitTargets.Add(hitbox.GetOwner());
+                dashAbilityRef.EndDash();
+            }
+        }
+    }
+
+    public void RequestDestroy() //Hello there!
+    {
+        Destroy(this.gameObject);
+    }
+}
