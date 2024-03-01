d2f86d609abcd7d4ada8ea7e3041f0d3b4e42370
greatbrick0 <speharley@gmail.com>
2024-03-01 18:35:08 -0500

    Added Gun Button
    Fixed bug where abilities would display ice damage numbers but were dealing normal type damage

--- Assets/General Enemy/Damageable.cs
+++ Assets/General Enemy/Damageable.cs
@@ -8,6 +8,13 @@ public abstract class Damageable : MonoBehaviour
     [field: SerializeField]
     public string team { get; set; }
 
-    //returns the amount of damage done (useful for lifesteal, vampirism, awarding points) 
+    /// <summary>
+    /// Attempts to do 
+    /// </summary>
+    /// <param name="damageAmount">The amount of damage </param>
+    /// <param name="sourceType"></param>
+    /// <param name="spotType"></param>
+    /// <param name="element"></param>
+    /// <returns>Returns the amount of damage done (useful for lifesteal, vampirism, awarding points). </returns>
     public abstract int TakeDamage(int damageAmount, DamageSource sourceType, DamageSpot spotType, DamageElement element = DamageElement.Normal);
 }
