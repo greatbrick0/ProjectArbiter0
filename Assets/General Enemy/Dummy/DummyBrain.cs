10cef90ff7708a87f25bccdaf65fc64f0ba50a1a
greatbrick <speharley@gmail.com>
2024-03-07 17:25:58 -0500

    Added Dummy Enemy

---
+++ Assets/General Enemy/Dummy/DummyBrain.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyMovement))]
+public class DummyBrain : EnemyBrain
+{
+    private EnemyMovement moveScript;
+
+    [SerializeField]
+    private bool returnToSpot = false;
+
+    private void Start()
+    {
+        moveScript = GetComponent<EnemyMovement>();
+    }
+
+    public override void Logic()
+    {
+        if (returnToSpot) moveScript.IdleBehaviour();
+        else moveScript.StandStill();
+    }
+}
