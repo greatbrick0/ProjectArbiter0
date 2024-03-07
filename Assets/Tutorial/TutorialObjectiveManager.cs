23a2dec60f70d413a5c8bb03d22af512bb5737ea
conorMaccarthy <conor.maccarthy@ontariotechu.net>
2024-03-07 16:51:12 -0500

    Made tutorial scene and basic tutorial scripts

---
+++ Assets/Tutorial/TutorialObjectiveManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialObjectiveManager : MonoBehaviour
+{
+    public int currentTutorialPrompt = 0;
+
+
+}
