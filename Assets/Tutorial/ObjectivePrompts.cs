e8f1db52d1c17d01e1f8fcdb28fec8ad7e2c3857
greatbrick0 <speharley@gmail.com>
2024-03-27 07:17:44 -0400

    Updated Tutorial Narrator
    The frame no longer appears when there is no text to show

--- Assets/Tutorial/ObjectivePrompts.cs
+++ Assets/Tutorial/ObjectivePrompts.cs
@@ -2,15 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ObjectivePrompts : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI objectiveText;
+    [SerializeField] GameObject objectiveFrame;
     [SerializeField] TutorialObjectiveManager tutorialManager;
     [SerializeField] List<string> tutorialPrompts;
 
+    int previousPrompt = -1;
+
     private void Update()
     {
-        objectiveText.text = tutorialPrompts[tutorialManager.currentTutorialPrompt];
+        if(previousPrompt != tutorialManager.currentTutorialPrompt)
+        {
+            ChangeText(tutorialPrompts[tutorialManager.currentTutorialPrompt]);
+            previousPrompt = tutorialManager.currentTutorialPrompt;
+        }
+    }
+
+    void ChangeText(string newText)
+    {
+        if(newText != "")
+        {
+            StopAllCoroutines();
+            objectiveText.text = newText;
+            objectiveFrame.GetComponent<Image>().color = ChangeColourAlpha(objectiveFrame.GetComponent<Image>().color, 1f);
+        }
+        else
+        {
+            StartCoroutine(SetClearDelayed(0.3f));
+        }
+    }
+
+    Color ChangeColourAlpha(Color oldColour, float newAlpha)
+    {
+        return new Color(oldColour.r, oldColour.g, oldColour.b, newAlpha);
+    }
+
+    IEnumerator SetClearDelayed(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        objectiveFrame.GetComponent<Image>().color = ChangeColourAlpha(objectiveFrame.GetComponent<Image>().color, 0.0f);
+        objectiveText.text = "";
     }
 }
