c396cfa2eab91c797bef8d9fae61fd9b5965c682
Gmanmondo <gabriel.mondelli@ontariotechu.net>
2024-03-28 16:12:36 -0400

    enemy health sound change

--- Assets/General Enemy/Health/EnemyHealth.cs
+++ Assets/General Enemy/Health/EnemyHealth.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using DamageDetails;
 using System.Collections;
 using System.Collections.Generic;
@@ -54,3 +55,73 @@ public class EnemyHealth : Damageable
         Destroy(this.gameObject);
     }
 }
+=======
+using DamageDetails;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyHealth : Damageable
+{
+    [field: SerializeField]
+    public int maxHealth { get; private set; } = 100;
+    [field: SerializeField]
+    public int health { get; private set; }
+
+    [SerializeField]
+    private float weakSpotMult = 2.0f;
+    [SerializeField]
+    private float armourSpotMult = 0.5f;
+    [SerializeField]
+    public List<DamageElement> ignoredElements = new List<DamageElement>();
+    [SerializeField]
+    public List<DamageSource> ignoredSources = new List<DamageSource>();
+
+
+    public delegate void EnemyDied();
+    public event EnemyDied enemyDied;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public override int TakeDamage(int damageAmount, DamageSource sourceType, DamageSpot spotType, DamageElement element = DamageElement.Normal)
+    {
+        int prevHealth = health;
+
+        if (ignoredElements.Contains(element) || ignoredSources.Contains(sourceType))
+        {
+            damageAmount = 0;
+        }
+        else BroadcastMessage("HurtAnim");
+        if (spotType == DamageSpot.Body)
+        {
+            damageAmount = Mathf.CeilToInt(damageAmount * armourSpotMult);
+            FMODUnity.RuntimeManager.PlayOneShotAttached(FMODEvents.instance.bodyHit, gameObject);
+        }
+        else if (spotType == DamageSpot.Head)
+        {
+            damageAmount = Mathf.FloorToInt(damageAmount * weakSpotMult);
+            FMODUnity.RuntimeManager.PlayOneShotAttached(FMODEvents.instance.citicalHit, gameObject);
+        }
+
+        health -= damageAmount;
+
+        if(health <= 0)
+        {
+            Die();
+            health = Mathf.Max(health, 0);
+        }
+        
+        return prevHealth - health;
+    }
+
+    void Die()
+    {
+        if(enemyDied != null) enemyDied();
+        Destroy(this.gameObject);
+    }
+}
+>>>>>>> Stashed changes
