e27b909ae621871c030d4bcb6f3f9bccaff284fa
greatbrick <speharley@gmail.com>
2024-04-02 06:11:23 -0400

    Invisible Laser Beam
    No visuals

--- Assets/General Enemy/LaserTurret/TurretLaserController.cs
+++ Assets/General Enemy/LaserTurret/TurretLaserController.cs
@@ -1,3 +1,4 @@
+using Coherence.Cloud;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,27 +15,90 @@ public class TurretLaserController : MonoBehaviour
     float timeCharging = 0.0f;
     int chargeState = 0;
 
+    
+
+    [SerializeField]
+    [Tooltip("How much the turret can rotate in each axis, measured in degrees per second.")]
+    private float turnSpeed = 90.0f;
+    [SerializeField]
+    [Tooltip("How much the turret can rotate each axis while charging the laser, measured in degrees per second.")]
+    private float slowTurnSpeed = 30.0f;
+    [Header("Beam")]
+    [SerializeField]
+    private float beamRadius = 0.5f;
+    [SerializeField]
+    private float beamLength = 300.0f;
+    [SerializeField]
+    private int beamDamage = 10;
+    private List<PlayerHealth> hitPlayers = new List<PlayerHealth>();
+    [SerializeField]
+    [Tooltip("How frequently a laser can repeatedly hit a player, measured in seconds. \n" +
+        "Lower numbers allow players to dodge out of some of the damage, but also make the beam feel more impactful.")]
+    private float hitRefreshTime = 0.2f;
+    private float timeSinceRefresh = 0.0f;
+
+
     private void Update()
     {
         if (charging) timeCharging += 1.0f * Time.deltaTime;
         else timeCharging = 0.0f;
 
+        //activations of animations and particle effects can go here
         if (timeCharging >= 5.0f && chargeState == 3) //end laser
         {
             timeCharging = 0.0f;
             chargeState = 0;
+            timeSinceRefresh = 0.0f;
+            hitPlayers.Clear();
         }
-        else if (timeCharging >= 4.6f && chargeState == 2) chargeState = 3; //start laser
-        else if (timeCharging >= 4.0f && chargeState == 1) chargeState = 2; //stop moving
-        else if (timeCharging >= 2.7f && chargeState == 0) chargeState = 1; //slow down
-        else if (timeCharging >= 0.0f) chargeState = 0; //default 
+        else if (timeCharging >= 4.6f && chargeState == 2) //start laser
+        {
+            chargeState = 3;
+            print("fire");
+        }
+        else if (timeCharging >= 4.0f && chargeState == 1)  //stop rotation
+        {
+            chargeState = 2;
+        }
+        else if (timeCharging >= 2.7f && chargeState == 0)  //slow rotation
+        {
+            chargeState = 1;
+        }
+        else //default 
+        {
+            chargeState = 0;
+        }
 
-        if(chargeState == 3) Laser();
+        if (chargeState == 3)
+        {
+            timeSinceRefresh += 1.0f * Time.deltaTime;
+            if (timeSinceRefresh >= hitRefreshTime)
+            {
+                timeSinceRefresh = 0.0f;
+                hitPlayers.Clear();
+            }
+            Laser();
+        }
     }
 
     private void Laser()
     {
+        bool didCollide;
+        int layers = (1 << 0) | (1 << 6) | (1 << 8);
 
+        ray = new Ray(head.position, head.forward);
+        didCollide = Physics.SphereCast(ray, beamRadius, out hit, beamLength, layers);
+
+        if (didCollide)
+        {
+            PlayerHealth p = hit.collider.gameObject.GetComponent<PlayerHealth>();
+            if (p != null)
+            {
+                if(hitPlayers.Contains(p)) return;
+                hit.collider.gameObject.GetComponent<PlayerHealth>().TakeDamage(beamDamage);
+                hitPlayers.Add(p);
+            } 
+        }
     }
 
     public bool CheckLosToPlayer(GameObject player, float checkRange)
