3a3068db05d12f6d09d7610fc63331b2174a8dc1
conorMaccarthy <conor.maccarthy@ontariotechu.net>
2024-04-03 19:32:12 -0400

    Fixed Practice Range, Added Return to Menu Button

--- Assets/coherence/baked/Definition.gen.cs
+++ Assets/coherence/baked/Definition.gen.cs
@@ -16,7 +16,7 @@ namespace Coherence.Generated
 
 	public class Definition : IDefinition
 	{
-		public const string schemaId = "f314420f56d585548cb869c84bcabe6bdb52d773";
+		public const string schemaId = "78111b648729010a7a58a9f58aa21a6b1635c688";
 		public const uint InternalWorldPosition = 0;
 		public const uint InternalWorldOrientation = 1;
 		public const uint InternalLocalUser = 2;
@@ -132,15 +132,16 @@ namespace Coherence.Generated
 		public const uint InternalAdoptOrphan = 3;
 		public const uint InternalPersistenceReady = 4;
 		public const uint InternalGenericCommand = 5;
-		public const uint InternalEnemy_7365a46dbef03be41b6173e6147d68d1_EnemySyncInit__char_46_SetReferences_b98d46b8_119e_4f49_9104_d0bbf89a99de = 6;
-		public const uint InternalEnemyA_fb33efce218f91746a8e1743d2d6c0f9_EnemySyncInit__char_46_SetReferences_5e717b57_f773_4c0b_8535_2b6f27c37857 = 7;
-		public const uint InternalEnemyB_5714c7c58551c2843bebf23f9feebbe2_EnemySyncInit__char_46_SetReferences_a36f68fc_d2ff_407e_b1bb_7a6268791147 = 8;
-		public const uint InternalEnemyC_75a6438d17923ed4bbf57e0d1b70a8fa_EnemySyncInit__char_46_SetReferences_59c44945_06c3_4fe7_81df_2a7111dcfc20 = 9;
-		public const uint InternalEnemyD_bced0933a9712ac4aac05cdf91d8f3f8_EnemySyncInit__char_46_SetReferences_722794ab_8faa_4e48_b5da_23968464a290 = 10;
-		public const uint InternalPlayer_9b83865ad432b2840a2ba6c6e5d0fada_AbilityInputSystem__char_46_SendCastToAbility_09631ba5_b8ad_4b21_8720_2dcef6abf6da = 11;
-		public const uint InternalPlayer_9b83865ad432b2840a2ba6c6e5d0fada_PlayerHealth__char_46_PlayerDown_8ed7af65_8faa_4df4_a8bc_d05c5ec8b2a1 = 12;
-		public const uint InternalPlayer_9b83865ad432b2840a2ba6c6e5d0fada_SanitySystem__char_46_ShowAura_9f663484_9c8c_491f_9c1e_c2dd9fe2d5c8 = 13;
-		public const uint InternalPlayer_9b83865ad432b2840a2ba6c6e5d0fada_WeaponHolder__char_46_Shoot_4580daad_388d_444f_92c3_72b98a4d593d = 14;
+		public const uint InternalDummyEnemy_03cae38ad2ee1be41926ecf791a4649a_EnemySyncInit__char_46_SetReferences_c90cdd83_0b27_4380_ac1d_212aec63515c = 6;
+		public const uint InternalEnemy_7365a46dbef03be41b6173e6147d68d1_EnemySyncInit__char_46_SetReferences_b98d46b8_119e_4f49_9104_d0bbf89a99de = 7;
+		public const uint InternalEnemyA_fb33efce218f91746a8e1743d2d6c0f9_EnemySyncInit__char_46_SetReferences_5e717b57_f773_4c0b_8535_2b6f27c37857 = 8;
+		public const uint InternalEnemyB_5714c7c58551c2843bebf23f9feebbe2_EnemySyncInit__char_46_SetReferences_a36f68fc_d2ff_407e_b1bb_7a6268791147 = 9;
+		public const uint InternalEnemyC_75a6438d17923ed4bbf57e0d1b70a8fa_EnemySyncInit__char_46_SetReferences_59c44945_06c3_4fe7_81df_2a7111dcfc20 = 10;
+		public const uint InternalEnemyD_bced0933a9712ac4aac05cdf91d8f3f8_EnemySyncInit__char_46_SetReferences_722794ab_8faa_4e48_b5da_23968464a290 = 11;
+		public const uint InternalPlayer_9b83865ad432b2840a2ba6c6e5d0fada_AbilityInputSystem__char_46_SendCastToAbility_09631ba5_b8ad_4b21_8720_2dcef6abf6da = 12;
+		public const uint InternalPlayer_9b83865ad432b2840a2ba6c6e5d0fada_PlayerHealth__char_46_PlayerDown_8ed7af65_8faa_4df4_a8bc_d05c5ec8b2a1 = 13;
+		public const uint InternalPlayer_9b83865ad432b2840a2ba6c6e5d0fada_SanitySystem__char_46_ShowAura_9f663484_9c8c_491f_9c1e_c2dd9fe2d5c8 = 14;
+		public const uint InternalPlayer_9b83865ad432b2840a2ba6c6e5d0fada_WeaponHolder__char_46_Shoot_4580daad_388d_444f_92c3_72b98a4d593d = 15;
 
 		private static readonly Dictionary<uint, string> componentNamesForTypeIds = new Dictionary<uint, string>() {
 			{ 0, "WorldPosition" },
@@ -741,6 +742,8 @@ namespace Coherence.Generated
 					return PersistenceReady.Deserialize(bitStream);
 				case Definition.InternalGenericCommand:
 					return GenericCommand.Deserialize(bitStream);
+				case Definition.InternalDummyEnemy_03cae38ad2ee1be41926ecf791a4649a_EnemySyncInit__char_46_SetReferences_c90cdd83_0b27_4380_ac1d_212aec63515c:
+					return DummyEnemy_03cae38ad2ee1be41926ecf791a4649a_EnemySyncInit__char_46_SetReferences_c90cdd83_0b27_4380_ac1d_212aec63515c.Deserialize(bitStream);
 				case Definition.InternalEnemy_7365a46dbef03be41b6173e6147d68d1_EnemySyncInit__char_46_SetReferences_b98d46b8_119e_4f49_9104_d0bbf89a99de:
 					return Enemy_7365a46dbef03be41b6173e6147d68d1_EnemySyncInit__char_46_SetReferences_b98d46b8_119e_4f49_9104_d0bbf89a99de.Deserialize(bitStream);
 				case Definition.InternalEnemyA_fb33efce218f91746a8e1743d2d6c0f9_EnemySyncInit__char_46_SetReferences_5e717b57_f773_4c0b_8535_2b6f27c37857:
@@ -837,6 +840,9 @@ namespace Coherence.Generated
 					break;
 				case Definition.InternalGenericCommand:
 					GenericCommand.Serialize((GenericCommand)data, bitStream);
+					break;
+				case Definition.InternalDummyEnemy_03cae38ad2ee1be41926ecf791a4649a_EnemySyncInit__char_46_SetReferences_c90cdd83_0b27_4380_ac1d_212aec63515c:
+					DummyEnemy_03cae38ad2ee1be41926ecf791a4649a_EnemySyncInit__char_46_SetReferences_c90cdd83_0b27_4380_ac1d_212aec63515c.Serialize((DummyEnemy_03cae38ad2ee1be41926ecf791a4649a_EnemySyncInit__char_46_SetReferences_c90cdd83_0b27_4380_ac1d_212aec63515c)data, bitStream);
 					break;
 				case Definition.InternalEnemy_7365a46dbef03be41b6173e6147d68d1_EnemySyncInit__char_46_SetReferences_b98d46b8_119e_4f49_9104_d0bbf89a99de:
 					Enemy_7365a46dbef03be41b6173e6147d68d1_EnemySyncInit__char_46_SetReferences_b98d46b8_119e_4f49_9104_d0bbf89a99de.Serialize((Enemy_7365a46dbef03be41b6173e6147d68d1_EnemySyncInit__char_46_SetReferences_b98d46b8_119e_4f49_9104_d0bbf89a99de)data, bitStream);
