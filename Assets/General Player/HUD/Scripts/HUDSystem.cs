e041ef21efef4ddba87b25a7222c55a71f25c47b
conorMaccarthy <conor.maccarthy@ontariotechu.net>
2024-04-18 12:40:21 -0400

    New Win and Lose Screens

--- Assets/General Player/HUD/Scripts/HUDSystem.cs
+++ Assets/General Player/HUD/Scripts/HUDSystem.cs
@@ -35,7 +35,18 @@ public class HUDSystem : MonoBehaviour
     [SerializeField]
     private GameObject loseScreen;
     [SerializeField]
+    private GameObject loseTextMain;
+    [SerializeField]
+    private GameObject loseTextReturning;
+    [SerializeField]
     private GameObject winScreen;
+    [SerializeField]
+    private GameObject winTextMain;
+    [SerializeField]
+    private GameObject winTextReturning;
+    [SerializeField]
+    private GameObject backgroundPanel;
+
     public LoadSceneButtonGeneral sceneLoader;
 
     public void UseAbility(int tier)
@@ -89,25 +100,96 @@ public class HUDSystem : MonoBehaviour
 
     public void GameOver()
     {
-        FindObjectOfType<AudioManager>().masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        for(int ii = 0; ii < transform.childCount; ii++)
-        {
-            transform.GetChild(ii).gameObject.SetActive(false);
-        }
-        loseScreen.SetActive(true);
-        Invoke(nameof(BackToMainMenu), 3.0f);
+        StartCoroutine(LoseSequence());
     }
 
-    public void WinGame()
+    public IEnumerator LoseSequence()
     {
         FindObjectOfType<AudioManager>().masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         for (int ii = 0; ii < transform.childCount; ii++)
         {
             transform.GetChild(ii).gameObject.SetActive(false);
         }
-        winScreen.SetActive(true);
-        Invoke(nameof(BackToMainMenu), 3.0f);
+
+        yield return new WaitForSeconds(1);
+        backgroundPanel.SetActive(true);
+        LeanTween.value(0, 0.6f, 2).setOnUpdate(FadeInBackground);
+
+        yield return new WaitForSeconds(2);
+        loseScreen.SetActive(true);
+        LeanTween.value(0, 1, 2).setOnUpdate(FadeInLoseScreen);
+
+        yield return new WaitForSeconds(2);
+        LeanTween.value(0, 1, 1).setOnUpdate(FadeInLoseTextMain);
+
+        yield return new WaitForSeconds(2);
+        LeanTween.value(0, 1, 1).setOnUpdate(FadeInLoseTextReturning);
+
+        yield return new WaitForSeconds(4);
+        BackToMainMenu();
     }
+
+    public void WinGame()
+    {
+        StartCoroutine(WinSequence());
+    } 
+
+    public IEnumerator WinSequence()
+    {
+        FindObjectOfType<AudioManager>().masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        for (int ii = 0; ii < transform.childCount; ii++)
+        {
+            transform.GetChild(ii).gameObject.SetActive(false);
+        }
+
+        yield return new WaitForSeconds(1);
+        backgroundPanel.SetActive(true);
+        LeanTween.value(0, 0.6f, 2).setOnUpdate(FadeInBackground);
+
+        yield return new WaitForSeconds(2);
+        winScreen.SetActive(true);
+        LeanTween.value(0, 1, 2).setOnUpdate(FadeInWinScreen);
+
+        yield return new WaitForSeconds(2);
+        LeanTween.value(0, 1, 1).setOnUpdate(FadeInWinTextMain);
+
+        yield return new WaitForSeconds(2);
+        LeanTween.value(0, 1, 1).setOnUpdate(FadeInWinTextReturning);
+
+        yield return new WaitForSeconds(4);
+        BackToMainMenu();
+    }
+
+    #region UI Fade Functions
+    private void FadeInBackground(float val)
+    {
+        backgroundPanel.GetComponent<Image>().color = new Color(0, 0, 0, val);
+    }
+    private void FadeInWinScreen(float val)
+    {
+        winScreen.GetComponent<Image>().color = new Color(0.1f, 0.16f, 0.48f, val);
+    }
+    private void FadeInWinTextMain(float val)
+    {
+        winTextMain.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, val);
+    }
+    private void FadeInWinTextReturning(float val)
+    {
+        winTextReturning.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, val);
+    }
+    private void FadeInLoseScreen(float val)
+    {
+        loseScreen.GetComponent<Image>().color = new Color(0.44f, 0.06f, 0.06f, val);
+    }
+    private void FadeInLoseTextMain(float val)
+    {
+        loseTextMain.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, val);
+    }
+    private void FadeInLoseTextReturning(float val)
+    {
+        loseTextReturning.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, val);
+    }
+    #endregion
 
     public void BackToMainMenu()
     {
